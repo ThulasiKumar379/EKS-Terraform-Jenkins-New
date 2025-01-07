dfe315bf50a9a988a812b398361074a06756592d
ThulasiKumar379 <Thulasikumar7411@gmail.com>
2025-01-07 07:55:29 +0530

    Updated required changes in provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
 locals {
   region = "us-east-1"
   name   = "thulasicloud-cluster"
-  vpc_cidr = "10.123.0.0/16"
+  vpc_cidr = "172.31.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  public_subnets  = ["172.31.1.0/24", "172.31.2.0/24"]
+  private_subnets = ["172.31.3.0/24", "172.31.4.0/24"]
+  intra_subnets   = ["172.31.5.0/24", "172.31.6.0/24"]
   tags = {
     Example = local.name
   }
